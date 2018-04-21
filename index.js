a79082d97dee44f3ae092cb0f07c9d600d99b10f
Sanskar <sansyrox@gmail.com>
2018-04-21 15:53:56 +0530

    Smooth UI

---
+++ index.js
@@ -0,0 +1,6 @@
+const API_KEY = 'AIzaSyCuVdfqTn6ahF6u-sPWYVL8Mvis6PGwa7g';
+const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=';
+
+$(document).ready(function(){
+    $(".middle").fadeOut(200).then($("#map").fadeIn(2000));
+});
