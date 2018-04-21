7fd760f48801c041ece74954559e4762097686d4
Karan Singh Bora <bora.karan5@gmail.com>
2018-04-22 04:44:48 +0530

    Pre final commit

--- jquery.js
+++ jquery.js
@@ -1,4 +1,6 @@
+/*global $ , document */
 $(document).ready(function(){
-    $(".middle").fadeOut(200).then($("#fadein").fadeIn(3000));
+    $(".middle").fadeOut(200);
+    $("#fadein").fadeIn(3000);
     $("#map").fadeIn();
 });
