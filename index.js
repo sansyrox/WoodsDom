659f48fa7c0d995d00f3ec045464afca886c588b
Nikhil Seth <rootedgy24@gmail.com>
2018-04-21 16:35:38 +0530

    Added Geolocation
    Geolocation to go to pin the map where user is located

--- index.js
+++ index.js
@@ -4,3 +4,37 @@ const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=';
 $(document).ready(function(){
     $(".middle").fadeOut(200).then($("#map").fadeIn(2000));
 });
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 60.169856, lng: 24.938379},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
